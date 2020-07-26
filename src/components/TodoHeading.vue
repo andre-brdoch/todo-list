d2bc0d02aef8b071630d42da91bf9e1f7b205033
Andre Brdoch <andrebrdoch@yahoo.de>
2020-07-26 18:28:24 +0200

    TodoHeading subcomponent

---
+++ src/components/TodoHeading.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="header">
+    <form v-show="inputIsVisible" @submit.prevent="updateHeading" class="form">
+      <input
+        v-model="heading"
+        @blur="updateHeading"
+        ref="input"
+        class="input"
+      />
+    </form>
+    <h3
+      v-show="!inputIsVisible"
+      @click.stop="showInput"
+      @focus.stop="showInput"
+      class="heading"
+    >
+      {{ heading }}
+    </h3>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeading",
+
+  props: {
+    name: { type: String, default: "New todo list" }
+  },
+
+  data() {
+    return {
+      heading: this.name,
+      inputIsVisible: false
+    };
+  },
+
+  methods: {
+    updateHeading() {
+      this.hideInput();
+    },
+    showInput() {
+      const { input } = this.$refs;
+      this.inputIsVisible = true;
+      // setTimeout to make it work on FF
+      setTimeout(() => {
+        input.focus();
+        input.select();
+        input.setSelectionRange(0, input.value.length);
+      }, 0);
+    },
+    hideInput() {
+      this.inputIsVisible = false;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  padding-bottom: 1.5rem;
+}
+.heading {
+  font-size: 2rem;
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,6 @@
 <template>
   <article class="todo">
-    <header class="header">
-      <form
-        v-show="headingInputIsVisible"
-        @submit.prevent="updateHeading"
-        class="heading-form"
-      >
-        <input
-          v-model="heading"
-          @blur="updateHeading"
-          ref="headingInput"
-          class="heading-input"
-        />
-      </form>
-      <h3
-        v-show="!headingInputIsVisible"
-        @click.stop="showHeadingInput"
-        @focus.stop="showHeadingInput"
-        class="heading"
-      >
-        {{ heading }}
-      </h3>
-    </header>
+    <todo-heading :name="name"></todo-heading>
     <ul class="list">
       <li v-for="(item, i) in list" :key="item.text" class="task">
         <input type="checkbox" :id="item.text" class="checkbox" />
@@ -43,8 +22,12 @@
 </template>
 
 <script>
+import TodoHeading from "./TodoHeading.vue";
+
 export default {
   name: "TodoList",
+
+  components: { TodoHeading },
 
   props: {
     name: { type: String, default: "New todo list" },
@@ -58,9 +41,7 @@ export default {
 
   data() {
     return {
-      newTask: "",
-      heading: this.name,
-      headingInputIsVisible: false
+      newTask: ""
     };
   },
 
@@ -73,22 +54,6 @@ export default {
     },
     deleteItem(i) {
       this.list.splice(i, 1);
-    },
-    updateHeading() {
-      this.hideHeadingInput();
-    },
-    showHeadingInput() {
-      const { headingInput } = this.$refs;
-      this.headingInputIsVisible = true;
-      // setTimeout to make it work on FF
-      setTimeout(() => {
-        headingInput.focus();
-        headingInput.select();
-        headingInput.setSelectionRange(0, headingInput.value.length);
-      }, 0);
-    },
-    hideHeadingInput() {
-      this.headingInputIsVisible = false;
     }
   }
 };
@@ -101,14 +66,7 @@ export default {
   border-radius: 3px;
   max-width: 40rem;
 }
-.header {
-  margin-bottom: 1.5rem;
-}
-.heading {
-  font-size: 2rem;
-  margin: 0;
-  padding: 0;
-}
+
 .list {
   list-style: none;
   margin: 0;

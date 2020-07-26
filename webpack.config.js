81286fd40b1cdbeaf118d7398dc6ebb8f3685e8a
Andre Brdoch <andrebrdoch@yahoo.de>
2020-07-26 17:08:07 +0200

    vue setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   mode: "production",
@@ -9,9 +10,11 @@ module.exports = {
     filename: "app.[hash].js",
     path: path.resolve(__dirname, "dist")
   },
+  resolve: { alias: { vue: 'vue/dist/vue.esm.js' } },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" })
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new VueLoaderPlugin()
   ],
   module: {
     rules: [
@@ -26,7 +29,11 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"]
-      }
+      },
+      {
+       test: /\.vue$/,
+       loader: 'vue-loader'
+     }
     ]
   }
 };

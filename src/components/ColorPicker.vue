2fa5511b743da2936890ec7c236ae07fb5682181
Andre Brdoch <andrebrdoch@yahoo.de>
2020-07-26 19:57:12 +0200

    ColorPicker component

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="color-picker">
+    <li
+      v-for="color in colors"
+      :key="color"
+      @click="changeColor(color)"
+      class="color"
+      :style="`background-color: ${color};`"
+    />
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ColorPicker",
+
+  data() {
+    return {
+      colors: ["#ffb900", "#f7630c", "#e81123", "#0078d7", "#107c10", "#881798"]
+    };
+  },
+
+  methods: {
+    changeColor(color) {
+      this.$emit("color-change", color);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.color-picker {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.color {
+  width: 1.5rem;
+  cursor: pointer;
+
+  &:after {
+    content: "";
+    display: block;
+    // square
+    padding-top: 100%;
+  }
+}
+</style>
--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,61 +1,43 @@
 <template>
   <footer class="footer">
-    <div @click.stop="showColorPicker" class="clr-btn">Change color</div>
-    <ul v-show="colorPickerIsVisible" class="color-picker">
-      <li
-        v-for="color in colors"
-        :key="color"
-        @click="$emit('color-change', color)"
-        class="color"
-        :style="`background-color: ${color};`"
-      />
-    </ul>
+    <div @click.stop="toggleColorPicker" class="clr-btn">
+      Change color
+    </div>
+    <color-picker
+      v-if="colorPickerIsVisible"
+      v-on:color-change="handleColorChange"
+    />
   </footer>
 </template>
 
 <script>
+import ColorPicker from "./ColorPicker.vue";
+
 export default {
   name: "TodoFooter",
 
+  components: { ColorPicker },
+
   data() {
     return {
-      colors: [
-        "#ffb900",
-        "#f7630c",
-        "#e81123",
-        "#0078d7",
-        "#107c10",
-        "#881798"
-      ],
       colorPickerIsVisible: false
     };
   },
 
   methods: {
-    showColorPicker() {
-      this.colorPickerIsVisible = true;
+    toggleColorPicker() {
+      this.colorPickerIsVisible = !this.colorPickerIsVisible;
+    },
+    handleColorChange(color) {
+      this.$emit("color-change", color);
+      this.colorPickerIsVisible = false;
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-.color-picker {
-  display: flex;
-  flex-wrap: wrap;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.color {
-  width: 1.5rem;
+.clr-btn {
   cursor: pointer;
-
-  &:after {
-    content: "";
-    display: block;
-    // square
-    padding-top: 100%;
-  }
 }
 </style>
